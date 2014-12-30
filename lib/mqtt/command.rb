d538abadfde53623ca3aea0e9baf438f27113f6b
soramugi <soramugi.chika@gmail.com>
2014-12-30 22:37:37 +0900

    add desc

--- lib/mqtt/command.rb
+++ lib/mqtt/command.rb
@@ -2,24 +2,24 @@ require 'thor'
 
 module MQTT
   class Command < Thor
-    class_option :mqtt_server, type: :string,  aliases: '-s'
+    class_option :mqtt_server, type: :string,  aliases: '-s',  desc: 'Uri to be connected, Is priority.'
     class_option :host,        type: :string,  aliases: '-h'
     class_option :port,        type: :numeric, aliases: '-P',  default: 1883
-    class_option :username,    type: :string,  aliases: '-u'
-    class_option :password,    type: :string,  aliases: '-p'
-    class_option :ssl,         type: :boolean, default: false
+    class_option :username,    type: :string,  aliases: '-u',  desc: 'Host and Port needs'
+    class_option :password,    type: :string,  aliases: '-p',  desc: 'Host and Port needs'
+    class_option :ssl,         type: :boolean, default: false, desc: 'Default: --no-ssl'
 
-    desc 'get [TOPIC]', 'getting topic.'
+    desc 'get [TOPIC]', 'Getting topic.'
     def get(topic)
       client.get(topic) do |t,m|
         puts "topic:[#{t}] message:[#{m}]"
       end
-    rescue Interrupt
+    rescue Interrupt # Ctrl-c
       puts ''
       puts 'Bye.'
     end
 
-    desc 'publish [TOPIC] [MESSAGE]', 'publish message.'
+    desc 'publish [TOPIC] [MESSAGE]', 'Publish message.'
     def publish(topic,message='')
       client.publish(topic, message.to_s.encode('UTF-8'))
       puts "success!  topic:[#{topic}] message:[#{message}]"
